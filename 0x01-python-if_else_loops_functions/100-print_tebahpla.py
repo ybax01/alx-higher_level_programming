45040614ceb40cdfc9fb38432602f9dd51d235f8
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-09-12 01:38:11 +0100

    py stuff

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def print_tebahpla():
-    for i in range(25, -1, -1):
-        print("{:c}{:c}".format(122 - i * 2, 90 - i * 2), end='')
+    for i in range(26):
+        if i % 2 == 0:
+            print(chr(122 - i), end='')
+        else:
+            print(chr(90 - (i - 1)), end='')
 
 print_tebahpla()
