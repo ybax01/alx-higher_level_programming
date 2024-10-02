a06d91fc4f0872f83f5e26f13d73bb0081d7d689
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-10-03 00:17:29 +0100

    az

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module that defines a Square class.
+"""
+
+class Square:
+    """
+    A class that represents a square.
+    """
+    def __init__(self, size):
+        """
+        Initialize the square with a given size.
+        
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
