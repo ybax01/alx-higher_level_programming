28f5958de7083e199435e53b5d16416bdaef0fb0
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-10-02 23:20:04 +0100

    az

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end=" ")
             cnt += 1
-        except IndexError:
-            pass
-        print("")
-        return cnt
+    except IndexError:
+        pass
+    print("")
+    return(cnt)
