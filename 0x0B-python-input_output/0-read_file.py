97a7dd6ae62a13cb0da92db25aa8843ab8d5d73a
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-10-15 20:32:52 +0100

    az

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
