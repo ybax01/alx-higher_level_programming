27dd6e4e99a12c22436b6a3f12fb3e13a805d6e6
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-10-15 20:33:49 +0100

    az

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+Module to read and print the contents of a text file (UTF8).
+"""
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
+    """Reads a text file (UTF8) and prints it to stdout.
+    
+    Args:
+        filename (str): The name of the file to be read. Defaults to an empty string.
+    """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
