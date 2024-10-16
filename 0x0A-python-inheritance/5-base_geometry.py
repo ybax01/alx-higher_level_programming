84f27b531cb1f210dd36deb6a83942a5a9bc3a5c
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-10-16 22:53:13 +0100

    az

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+Module: 5-base_geometry
+This module defines the BaseGeometry class.
+"""
+
 class BaseGeometry:
+    """
+    A class used as a base for geometric shapes.
+
+    Attributes:
+        None
+    """
+
     pass
