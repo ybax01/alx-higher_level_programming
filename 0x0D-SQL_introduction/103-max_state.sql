6c93b52e4a90e3f6026fbd957e6798339b1eabdc
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-11-20 18:34:12 +0100

    sql

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,11 @@
+-- Display the maximum temperature of each state ordered by state name
+
+SELECT 
+    state, 
+    MAX(temperature) AS max_temp
+FROM 
+    temperatures
+GROUP BY 
+    state
+ORDER BY 
+    state;
