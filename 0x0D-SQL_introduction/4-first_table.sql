34f3a1ae6e35f165170ffc21edeb55a035337561
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-11-20 15:01:26 +0100

    sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Script to create the table first_table in the current database
+
+-- Create the table if it does not already exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
