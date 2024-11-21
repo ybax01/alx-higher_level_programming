f5a478777fad20b6113cb798773ffc920c3254d6
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-11-21 23:37:49 +0100

    sql 2

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,9 @@
+-- List all shows not linked to the genre Comedy
+SELECT title
+FROM tv_shows
+WHERE id NOT IN (
+    SELECT show_id
+    FROM tv_show_genres
+    WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy')
+)
+ORDER BY title ASC;
