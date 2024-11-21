d5834b95bbae19c909c1fe8d2fb4a1308b99dc01
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-11-21 22:37:45 +0100

    sql 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,12 @@
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on the database to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Apply the changes
+FLUSH PRIVILEGES;
+
