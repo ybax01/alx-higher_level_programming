61111d36dd40f32811e5599b1545fd62a9011570
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-11-21 23:32:33 +0100

    sql 2

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Select titles and genre IDs for shows with at least one linked genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
