caa1e5a7561d32a1cbe67371209c7119b00f401d
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-11-21 22:36:02 +0100

    sql 2

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Create user if not exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply the changes immediately
+FLUSH PRIVILEGES;
+
