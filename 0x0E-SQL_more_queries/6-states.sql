b6cb9c92b53ad76657d488cb32018965bad33a0f
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-11-21 23:00:43 +0100

    sql 2

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the created database
+USE hbtn_0d_usa;
+
+-- Create the table states if it doesn't exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
