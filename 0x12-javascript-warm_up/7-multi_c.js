49d13c5be68e22010285990d9bdf0933cca0c5eb
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-12-16 22:01:53 +0100

    js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by
-// using an array of string and a loop
+// Write a script that prints x times “C is fun”
 
 const lang = 'C is fun';
 
