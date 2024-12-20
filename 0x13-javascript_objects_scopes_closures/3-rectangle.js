2b9812d739444d15744321b4a2994a48ca5a5540
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-12-20 15:36:48 +0100

    az

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,8 +10,7 @@ module.exports = class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.height; j++) { console.log('x'); }
-      console.log('\n');
+	    console.log('X'.repeat(this.width));
     }
   }
 };
