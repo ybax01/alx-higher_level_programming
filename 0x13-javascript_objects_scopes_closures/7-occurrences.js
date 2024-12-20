d5a400d899a4c211edac7b928b8435dcb923d247
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-12-20 18:05:04 +0100

    az

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,6 +3,6 @@
 exports.nbOccurences = function (list, searchElement) {
   if (list === undefined || searchElement === undefined) return 0;
   let i = 0;
-	list.forEach((element) => { if (element === searchElement) i++; });
+  list.forEach((element) => { if (element === searchElement) i++; });
   return i;
 };
