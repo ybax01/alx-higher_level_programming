5ffd7013e63675fa8d4402888f172adb7c21f5dc
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-02-11 17:16:44 +0100

    az

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+const fs = require('fs');
+fs.writeFile(filePath, 'utf8', content, function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+  }
+});
